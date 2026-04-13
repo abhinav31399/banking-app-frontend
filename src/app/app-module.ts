8ed41bff7824ecdf26081f772acc4b6f405bfc80
apple <apple@apples-MacBook-Pro.local>
2026-04-13 11:09:27 +0530

    Initial commit - Angular project setup

---
+++ src/app/app-module.ts
@@ -0,0 +1,14 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { AccountList } from './account-list/account-list';
+
+@NgModule({
+  declarations: [App, AccountList],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
+})
+export class AppModule {}
